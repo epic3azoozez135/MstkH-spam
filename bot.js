7e0852c3a141be1ff7be405aa7e8f6ffd99912ac
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-29 02:50:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,16 @@ var channel = "528258301516054539";//ايدي الروم
     client.guilds.get(server).channels.get(channel).send('** - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH - MstkH **')
     },305);
 })
+
+client.on('ready', () => {
+  console.log(Logged in as !);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Hi') {
+    msg.reply('Hello');
+  }
+});
 client2.on('ready', async() => {
 var server = "528258300542713867"; // ايدي السررفر
 var channel = "528258301516054539";//ايدي الروم
